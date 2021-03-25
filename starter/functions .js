889668e90f2c376f4427bab60a54e080b3ff9ceb
luul negasi <luultesfay24@gmail.com>
2021-03-24 21:32:18 -0700

    challenge solved

--- starter/functions .js
+++ starter/functions .js
@@ -429,3 +429,23 @@ boardingPassangers(150, 3);
 -we are now boarding 150 passangers
 -we will board in 3 group ,each with 50 passengers
 */
+
+///challenge #
+/*Your tasks:
+1. Take the IIFE below and at the end of the function, attach an event listener that
+changes the color of the selected h1 element ('header') to blue, each time
+the body element is clicked. Do not select the h1 element again!
+
+(function () {
+const header = document.querySelector('h1');
+header.style.color = 'red';
+})();
+GOOD LUCK 😀*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
